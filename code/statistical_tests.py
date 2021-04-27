db74c3a41c12fca990fd1fb131c821fde053b527
ChamiLamelas <chami.lamelas@gmail.com>
2021-04-27 09:16:20 -0400

    changed to np style docs

--- code/statistical_tests.py
+++ code/statistical_tests.py
@@ -6,19 +6,21 @@ def f_test(sample_x, sample_y, larger_varx_alt):
     """
     Computes the F-value and corresponding p-value for a pair of samples and alternative hypothesis.
 
-    Parameters:
-        sample_x : list
-            A random sample x1,...,xnx. Let its (underlying) variance be ox^2 and its sample variance Sx^2. 
-        sample_y : list
-            A random sample y1,...,yny. Let its (underlying) variance be oy^2 and its sample variance Sy^2.
-        larger_varx_alt : bool
-            True if alternative hypothesis is ox^2 > oy^2. False if ox^2 < oy^2. 
+    Parameters
+    ----------
+    sample_x : list
+        A random sample x1,...,xnx. Let its (underlying) variance be ox^2 and its sample variance Sx^2. 
+    sample_y : list
+        A random sample y1,...,yny. Let its (underlying) variance be oy^2 and its sample variance Sy^2.
+    larger_varx_alt : bool
+        True if alternative hypothesis is ox^2 > oy^2. False if ox^2 < oy^2. 
 
-    Returns:
-        f_value : float
-            Sx^2 / Sy^2 as defined in 'A Quick, Compact, Two-Sample Dispersion Test: Count Five'.
-        p_value : 
-            Let F be the F-distribution with nx, ny df. 1 - P(F < f_value) if larger_varx_alt = True, P(F < f_value) otherwise. More extreme F = Sx^2 / Sy^2 values for alternative ox^2 > oy^2 are to the right. More extreme F values for ox^2 < oy^2 are to the left. 
+    Returns
+    -------
+    f_value : float
+        Sx^2 / Sy^2 as defined in 'A Quick, Compact, Two-Sample Dispersion Test: Count Five'.
+    p_value : 
+        Let F be the F-distribution with nx, ny df. 1 - P(F < f_value) if larger_varx_alt = True, P(F < f_value) otherwise. More extreme F = Sx^2 / Sy^2 values for alternative ox^2 > oy^2 are to the right. More extreme F values for ox^2 < oy^2 are to the left. 
     """
 
     # calculate unbiased sample variances (n-1 in the denominator)
@@ -41,17 +43,19 @@ def f1_test(sample_x, sample_y, larger_varx_alt):
     """
     Computes the F1-value as defined in 'Fixing the F Test for Equal Variances' and corresponding p-value for a pair of samples and alternative hypothesis. 
 
-    Parameters:
-        sample_x : list
-            A random sample x1,...,xnx. Let its (underlying) variance be ox^2 and its sample variance Sx^2. 
-        sample_y : list
-            A random sample y1,...,yny. Let its (underlying) variance be oy^2 and its sample variance Sy^2.
-        larger_varx_alt : bool
-            True if alternative hypothesis is ox^2 > oy^2. False if ox^2 < oy^2. 
+    Parameters
+    ----------
+    sample_x : list
+        A random sample x1,...,xnx. Let its (underlying) variance be ox^2 and its sample variance Sx^2. 
+    sample_y : list
+        A random sample y1,...,yny. Let its (underlying) variance be oy^2 and its sample variance Sy^2.
+    larger_varx_alt : bool
+        True if alternative hypothesis is ox^2 > oy^2. False if ox^2 < oy^2. 
 
-    Returns:
-        p_value : 
-            Let F be the F-distribution with rx, ry df as specified in equation (1) of 'Fixing the F Test for Equal Variances'. 1 - P(F < f_value) if larger_varx_alt = True, P(F < f_value) otherwise. 
+    Returns
+    -------
+    p_value : 
+        Let F be the F-distribution with rx, ry df as specified in equation (1) of 'Fixing the F Test for Equal Variances'. 1 - P(F < f_value) if larger_varx_alt = True, P(F < f_value) otherwise. 
     """
 
     # calculate unbiased sample variances (n-1 in the denominator)
@@ -85,21 +89,24 @@ def count_five(sample_x, sample_y, center):
     """
     Computes the extreme counts for samples x and y as defined in 'A Quick, Compact, Two-Sample Dispersion Test: Count Five'.
 
-    Parameters:
+    Parameters
+    ----------
         sample_x : list
             A random sample x1,...,xn. 
         sample_y : list
             A random sample y1,...,ym.
         center : str
             Whether to use 'mean' or 'median' for centering. 
-    Returns:
+    Returns
+    -------
         extreme_count_x : int
             C_x computed with centering mu being sample mean if center = 'mean' and sample median if center = 'median' as defined in equation (1) of 'A Quick, Compact, Two-Sample Dispersion Test: Count Five'.
         extreme_count_y : int
             C_y defined analogously to C_x above.
-    Raises:
-        ValueError
-            If center is neither 'mean' or 'median'.
+    Raises
+    ------
+    ValueError
+        If center is neither 'mean' or 'median'.
     """
 
     if center not in {'mean', 'median'}:
